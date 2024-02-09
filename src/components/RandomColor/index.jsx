736cf112ed18318e820d5bb85b549192c93ea1c1
Walter <2760walter@gmail.com>
2024-02-09 12:09:00 +0300

    generate colors

--- src/components/RandomColor/index.jsx
+++ src/components/RandomColor/index.jsx
@@ -1,15 +1,70 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const RandomColor = () => {
-    const [colortype, setColorType] = useState('hex')
-    const [color, setColor]= useState('#000000')
-    
+  const [colortype, setColorType] = useState("hex");
+  const [color, setColor] = useState("#000000");
+
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function handleCreateRandomHexColor() {
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+
+    setColor(hexColor);
+  }
+  function handleCreateRandomRgbColor() {
+    const r = randomColorUtility(256);
+    const g = randomColorUtility(256);
+    const b = randomColorUtility(256);
+
+    setColor(`rgb(${r},${g},${b})`);
+  }
+
+  useEffect(() => {
+    if (colortype === "rgb") handleCreateRandomRgbColor();
+    else handleCreateRandomHexColor();
+  }, [colortype]);
+
+  console.log(colortype);
 
   return (
-    <div className="container" style={{width:'100vw', height:'100vh', background:color}}>
-      <button>Create HEX Color</button>
-      <button>Create RGB Color</button>
-      <button>Generate Random Color</button>
+    <div
+      className="container"
+      style={{ width: "100vw", height: "100vh", background: color }}
+    >
+      <button onClick={() => setColorType("hex")}>Create HEX Color</button>
+      <button onClick={() => setColorType("rgb")}>Create RGB Color</button>
+      <button
+        onClick={
+          colortype === "hex"
+            ? handleCreateRandomHexColor
+            : handleCreateRandomRgbColor
+        }
+      >
+        Generate Random Color
+      </button>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          color: "#fff",
+          fontSize: "60px",
+          justifyContent: "center",
+          marginTop: "50px",
+          alignItems: "center",
+        }}
+      >
+        <h3>{colortype === "rgb" ? "RGB COLOR" : "HEX COLOR"}</h3>
+        <h1>{color}</h1>
+      </div>
     </div>
   );
 };
